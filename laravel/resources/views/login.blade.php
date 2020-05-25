2e940a03d30d63b54f0ee4f0649a85a6d1ec77e0
Suzuki Taichi <jc777pcm@gmail.com>
2020-05-25 15:00:01 +0900

    deve_suzuki2020/5/25

---
+++ laravel/resources/views/login.blade.php
@@ -0,0 +1,10 @@
+<!-- top -->
+@extends('layouts.layout_login')
+
+@section('title','ミールでMeal_LoginPage')
+@section('description','ディスクリプション')
+@include('layouts.head')
+
+@section('content')
+
+@endsection

5addf89dae26dfeb21b91852bbd1cd0f2b3530ad
Suzuki Taichi <jc777pcm@gmail.com>
2020-05-25 10:58:06 +0900

    deve_suzuki

--- laravel/resources/views/layouts/layout.blade.php
+++ laravel/resources/views/layouts/layout.blade.php
@@ -8,16 +8,20 @@
 <body>
     <header>
         <h1 id="title">header 共通部分</h1>
-        <button type=“button” onclick="location.href='/'">TopPage</button>
-        <button type=“button” onclick="location.href='/setting'">SettingPage</button>
-        <button type=“button” onclick="location.href='/foodrecode'">FoodRecodePage</button>
+        <table>
+        <tr>
+        <td><button type=“button” onclick="location.href='/'">TopPage</button></td>
+        <td><button type=“button” onclick="location.href='/setting'">SettingPage</button></td>
+        <td><button type=“button” onclick="location.href='/foodrecode'">FoodRecodePage</button></td>
+        </tr>
+        </table>
         <!-- <button type="button" onclick="history.back()">戻る</button> -->
     </header>
     <div id="wrapper">
         @yield('content')
     </div>
-    <footer>
+<!--     <footer>
     <h1>footer 共通部分</h1>
-    </footer>
+    </footer> -->
 </body>
 </html>

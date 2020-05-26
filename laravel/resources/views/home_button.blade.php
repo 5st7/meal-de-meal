4f5855a9d4ea4f641e108f230f7c9c70f403aae6
ghost7 <not404friends@gmail.com>
2020-05-26 14:28:56 +0900

    Update UserPage

---
+++ laravel/resources/views/home_button.blade.php
@@ -0,0 +1,1 @@
+<button href="/foodrecode" type="button" class="btn btn-warning btn-block">食材登録</button>
--- laravel/resources/views/top.blade.php
+++ laravel/resources/views/top.blade.php
@@ -2,15 +2,20 @@
 @section('title', '一覧')
 
 @section('content')
-  <div class="container float-center w-50">
-    <div class="list-group-flush">
-      @foreach($mials as $meal)
-      <div class="list-group-item">
-        <i class="far fa-image fa-2x mr-4 grey p-3 white-text rounded-circle" aria-hidden="true"></i>
-        {{$meal->meal_name}}
-        <span class="float-right">賞味期限:{{$meal->meal_limitday->format('Y/m/d H:i')}}</span>
+  <div class="container">
+    <div class="row">
+      <div class="mx-auto col col-12 col-sm-11 col-md-9 col-lg-7 col-xl-6">
+        @include('home_button')
+        <div class="list-group-flush">
+          @foreach($mials as $meal)
+          <div class="list-group-item">
+          <i class="far fa-image fa-2x mr-4 grey p-3 white-text rounded-circle" aria-hidden="true"></i>
+          {{$meal->meal_name}}
+          <span class="float-right">賞味期限:{{$meal->meal_limitday->format('Y/m/d H:i')}}</span>
+          </div>
+          @endforeach
+        </div>
       </div>
-      @endforeach
     </div>
   </div>
 @endsection

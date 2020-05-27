8567108449c7bf3252c30c758674e3d7e1691ea3
ghost7 <not404friends@gmail.com>
2020-05-28 08:10:21 +0900

    Add OneSignal

--- laravel/resources/views/app.blade.php
+++ laravel/resources/views/app.blade.php
@@ -14,6 +14,15 @@
   <!-- Material Design Bootstrap -->
   <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.11/css/mdb.min.css" rel="stylesheet">
   <link rel="stylesheet" href="CSS/sample.css">
+  <script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js" async=""></script>
+  <script>
+    var OneSignal = window.OneSignal || [];
+    OneSignal.push(function() {
+      OneSignal.init({
+        appId: "3f264d86-5e75-48e4-a725-7762be1c8c84",
+      });
+    });
+  </script>
 </head>
 
 <body>

ed57bb55cb054fb15d792011f8d54f302e577501
ghost7 <not404friends@gmail.com>
2020-05-29 12:03:45 +0900

    レイアウトの微調整

--- laravel/resources/views/alert.blade.php
+++ laravel/resources/views/alert.blade.php
@@ -9,8 +9,8 @@
       <div class="mx-auto col col-12 col-sm-11 col-md-9 col-lg-7 col-xl-6">
         @include('home_button')
         @foreach($meals ?? '' as $meal) {{--この行を追加--}} 
-        <div class="card mt-3" style="padding: 0px;background-color: #FFFDE7; justify-content-between">
-          <div class="card-body" style="margin: 0px;padding: 10px;background-color: #6D4C41;">
+        <div class="card mt-3 justify-content-between" style="background-color:#FFFDE7;">
+          <div class="card-body food" style="margin:0px;">
             <div d-flex justify-content-center>
               <div class="fo nt-weight-bold" style="font-size: 18px;
                 color: #FFFDE7;">
@@ -18,7 +18,7 @@
               </div> 
             </div>
           </div>
-          <div class="card-body pt-0 pb-2" style="border:none;">
+          <div class="card-body pt-0 pb-2" style="border:none; background-color:#FFFDE7;">
           <div class="d-flex justify-content-between">
             <div>
               <img id="meal" src="/storage/{{$meal->meal_image}}" class="rounded-circle">
